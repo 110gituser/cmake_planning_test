6d5767370fb35767c4ff0fbf2f23bcc6f7eae6b2
110gituser <2516483574@qq.com>
2025-05-05 18:01:36 +0800

    version 0.0.1

---
+++ planning_main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"process.h"
+using std::cout,std::endl;
+
+int main(){
+    cout<<"planning start\n";
+    Process pro;
+    pro.planProcess();
+    cout<<"planning end\n";
+    return 0;
+}
